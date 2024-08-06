34a9050d4b046bd308b11e4cff67787e61a48ed3
kurtis <xk233qwq@163.com>
2024-08-06 16:39:25 +0800

    function

--- src/syntax/function.c
+++ src/syntax/function.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
 int sum(int a, int b); // 声明一个函数原型
+void change(int*);
 
 void main()
 {
-    printf("%d + %d = %d", 1, 1, sum(1, 1));
+    printf("%d + %d = %d\n", 1, 1, sum(1, 1));
+
+    int num = 10;
+    printf("num: %d\n", num);
+    change(&num); // & 取地址
+    printf("num: %d\n", num);
 }
 
-int sum(const int a, const int b) // 定义函数的实现
+int sum(int a, int b) // 定义函数的实现
 {
     return a + b;
+}
+
+void change(int* i)
+{
+    *i = 1; // * 获取指针指向的变量
 }
